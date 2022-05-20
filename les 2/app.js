723565ec6c2e12621faaf4537b041468021a3796
Brandonvde010 <brandonvanderent010@gmail.com>
2022-05-20 12:15:52 +0200

    oefening 2 - Move the bird - 2

--- les 2/app.js
+++ les 2/app.js
@@ -7,3 +7,23 @@ bird.addEventListener("click", function() {
     bird.style.left = count + "px";
 })
 
+
+window.addEventListener("keydown", function(e){
+    if(e.key == "ArrowLeft") {
+        count -= 50;
+        bird.style.left = count + "px";
+    }
+    if(e.key == "ArrowRight") {
+        count += 50;
+        bird.style.left = count + "px";
+    }
+    if(e.key == "ArrowUp") {
+        count += 50;
+        bird.style.bottom = count + "px";
+    }
+    if(e.key == "ArrowDown") {
+        count += 50;
+        bird.style.top = count + "px";
+    }
+    })
+
